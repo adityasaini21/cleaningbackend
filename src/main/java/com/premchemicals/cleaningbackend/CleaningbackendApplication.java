11c10e289d9064f76c8e1b9b38dc5ebc04ac4206
adityasaini21 <2k22.cscys.2212326@gmail.com>
2026-09-20 12:18:53 +0530

    Indian time updates

--- src/main/java/com/premchemicals/cleaningbackend/CleaningbackendApplication.java
+++ src/main/java/com/premchemicals/cleaningbackend/CleaningbackendApplication.java
@@ -1,16 +1,21 @@
 package com.premchemicals.cleaningbackend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class CleaningbackendApplication {
 
+	@PostConstruct
+	public void init() {
+		// Set JVM default timezone to Indian Standard Time (IST)
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(CleaningbackendApplication.class, args);
 	}
-
 
 }
